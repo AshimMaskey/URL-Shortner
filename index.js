a682b2b7f4beed79a096afc719d8e87964675e25
Ashim Maskey <ashimmaskey4@gmail.com>
2025-02-19 11:52:59 +0545

    2nd commit: added server side rendering using ejs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express=require('express');
 const urlRoute=require('./routes/url');
 const indexRoute=require('./routes/index');
+const staticRoute=require('./routes/staticRouter');
 const {connectToDb}=require('./connect');
+const URL=require('./models/url');
+const path=require('path');
+
 
 const app=express();
 const PORT=8001;
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 
 //db connection
 connectToDb('mongodb://127.0.0.1:27017/short-url')
@@ -14,6 +23,7 @@ connectToDb('mongodb://127.0.0.1:27017/short-url')
 .catch((err)=>console.log('some error occured', err));
 
 app.use('/url', urlRoute);
-app.use('/', indexRoute);
+app.use('/url', indexRoute);
+app.use('/',staticRoute);
 
 app.listen(PORT, ()=> console.log(`Server started at port: ${PORT}`));
